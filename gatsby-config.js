ede43c34813bdc7a5a10c2759e0a4e1c23cf8866
Joshua Kennedy <josh@outthinkgroup.com>
2019-07-18 21:36:54 -0400

    cleared the cache

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,7 +78,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-offline`,
+    //`gatsby-plugin-offline`,
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
