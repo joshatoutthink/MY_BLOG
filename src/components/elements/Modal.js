8c907c919abdc94423d9720bf72d37fac68bfb57
Joshua Kennedy <josh@outthinkgroup.com>
2019-07-19 11:09:50 -0400

    changed portal component in to a hook and functional component

--- src/components/elements/Modal.js
+++ src/components/elements/Modal.js
@@ -7,7 +7,7 @@ import Icon from "./Icon"
 
 const Modal = ({ children, toggle, on }) => {
   return (
-    <Portal>
+    <Portal id="portal">
       {on && (
         <ModalWrapper>
           <Card>
